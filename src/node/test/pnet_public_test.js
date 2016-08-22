b0ed83849e3787ca375b8de703f6e95c2b11924a
Brent Dowdy <bdowdy@santanderconsumerusa.com>
2016-08-22 11:57:43 -0500

    removing apikey constructory param from public unit test

--- src/node/test/pnet_public_test.js
+++ src/node/test/pnet_public_test.js
@@ -4,15 +4,16 @@ var PNetPlus = require('./../pnetplus');
 var chaiHttp = require('chai-http');
 
  describe('Public Tests', function() {
+     
      it("pnet.blog.get()", function(done) {
-         var pnet = new PNetPlus('4568843eee').pnet;
+         var pnet = new PNetPlus().pnet;
          pnet.blog.get(function(err, data) {
             expect(data.length).to.not.equal(0).then(done());
          });
      });
      
      it("pnet.blog.item.get()", function(done) {
-         var pnet = new PNetPlus('sdgagh35').pnet;
+         var pnet = new PNetPlus().pnet;
          pnet.blog.item.get('1471829356', function(err, data) {
              var obj = JSON.parse(data.toString());
              expect(obj['id']).to.equal('1471829356');
